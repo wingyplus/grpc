8830d2adec451a0d9aa268ec40c4814b6a63add4
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2022-06-02 23:14:01 +0700

    Update deps

--- test/support/helloworld.pb.ex
+++ test/support/helloworld.pb.ex
@@ -2,11 +2,6 @@ defmodule Helloworld.HelloRequest do
   @moduledoc false
   use Protobuf, syntax: :proto3
 
-  @type t :: %__MODULE__{
-          name: String.t()
-        }
-  defstruct [:name]
-
   field :name, 1, type: :string
 end
 
@@ -14,30 +9,17 @@ defmodule Helloworld.HelloReply do
   @moduledoc false
   use Protobuf, syntax: :proto3
 
-  @type t :: %__MODULE__{
-          message: String.t()
-        }
-  defstruct [:message]
-
   field :message, 1, type: :string
 end
 
 defmodule Helloworld.HeaderRequest do
   @moduledoc false
   use Protobuf, syntax: :proto3
-
-  @type t :: %__MODULE__{}
-  defstruct []
 end
 
 defmodule Helloworld.HeaderReply do
   @moduledoc false
   use Protobuf, syntax: :proto3
-
-  @type t :: %__MODULE__{
-          authorization: String.t()
-        }
-  defstruct [:authorization]
 
   field :authorization, 1, type: :string
 end
